9cd94100917c57a82b1283f56b2f9df0aff4312d
Felipe Cecagno <fcecagno@gmail.com>
2016-04-08 08:30:38 -0300

    Revert "fetch the current data from the server when it starts"
    This reverts commit f27f70324a788d732471f0290992d0de1443c430.

--- bbb-live-monitor.rb
+++ bbb-live-monitor.rb
@@ -4,11 +4,6 @@
 require 'rubygems'
 require 'redis'
 require 'json'
-require 'nokogiri'
-require 'digest/sha1'
-require 'net/http'
-require 'json'
-require 'uri'
 
 def every_so_many_seconds(seconds)
   last_tick = Time.now
@@ -34,93 +29,6 @@ $stats = {
 }
 
 STDOUT.sync = true
-
-def read_salt
-  properties = Hash[File.read('/var/lib/tomcat7/webapps/bigbluebutton/WEB-INF/classes/bigbluebutton.properties').scan(/(.+?)=(.+)/)]
-  if properties.nil? or properties.empty?
-    return nil
-  else
-    return properties["securitySalt"]
-  end
-end
-
-def get_url(url)
-  begin
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) { |http|
-      http.request(req)
-    }
-    return res.body
-  rescue
-    return nil
-  end
-end
-
-def get_meetings(salt)
-  params = "random=#{rand(99999)}"
-  checksum = Digest::SHA1.hexdigest "getMeetings#{params}#{salt}"
-  url = URI.parse("http://localhost:8080/bigbluebutton/api/getMeetings?#{params}&checksum=#{checksum}")
-  return get_url(url)
-end
-
-def get_meeting_info(meeting_id, salt)
-  params = "meetingID=#{URI.escape(meeting_id)}&random=#{rand(99999)}"
-  checksum = Digest::SHA1.hexdigest "getMeetingInfo#{params}#{salt}"
-  url = URI.parse("http://localhost:8080/bigbluebutton/api/getMeetingInfo?#{params}&checksum=#{checksum}")
-  return get_url(url)
-end
-
-def init
-  salt = read_salt
-  return if salt.nil?
-
-  get_meetings_answer = get_meetings(salt)
-  return if get_meetings_answer.nil?
-
-  get_meetings_xml = Nokogiri::XML(get_meetings_answer)
-  return if get_meetings_xml.at_xpath('/response/returncode').text != "SUCCESS"
-  
-  get_meetings_xml.xpath('/response/meetings/meeting').each do |meeting|
-    meeting_id = meeting.at_xpath('meetingID').text
-    meeting = {
-      :users => {}
-    }
-    $meetings[meeting_id] = meeting
-
-    get_meeting_info_answer = get_meeting_info(meeting_id, salt)
-    next if get_meeting_info_answer.nil?
-
-    get_meeting_info_xml = Nokogiri::XML(get_meeting_info_answer)
-    return if get_meeting_info_xml.at_xpath('/response/returncode').text != "SUCCESS"
-    
-    get_meeting_info_xml.xpath('/response/attendees/attendee').each do |attendee|
-      userid = attendee.at_xpath('userID').text
-      user = {
-        :listenOnly => attendee.at_xpath('isListeningOnly').text.downcase == 'true',
-        :voiceUser => attendee.at_xpath('hasJoinedVoice').text.downcase == 'true',
-        :videos => [],
-        :bot => attendee.at_xpath('fullName').text.downcase.start_with?('bot')
-      }
-      attendee.xpath('videoStreams/streamName').each do |streamName|
-        user[:videos] << streamName.text
-      end
-      $meetings[meeting_id][:users][userid] = user
-    end
-  end
-  
-  update_stats
-end
-
-def update_stats
-  $stats[:num_meetings] = $meetings.length
-  $stats[:num_users] = $meetings.inject(0) { |total, (k, v)| total + v[:users].length}
-  $stats[:num_bots] = $meetings.inject(0) { |total, (k, v)| total + v[:users].values.select { |u| u[:bot] }.length }
-  $stats[:num_voice_participants] = $meetings.inject(0) { |total, (k, v)| total + v[:users].values.select { |u| u[:voiceUser] }.length }
-  $stats[:num_voice_listeners] = $meetings.inject(0) { |total, (k, v)| total + v[:users].values.select { |u| u[:listenOnly] }.length }
-  $stats[:num_videos] = $meetings.inject(0) { |total, (k, v)| total + v[:users].inject(0) { |total, (k, v)| total + v[:videos].length}}
-end
-
-init
 
 puts "date,num_meetings,num_users,num_bots,num_voice_participants,num_voice_listeners,num_videos"
 Thread.new do
@@ -185,6 +93,11 @@ $redis.subscribe('bigbluebutton:from-bbb-apps:meeting', 'bigbluebutton:from-bbb-
       end
     end
 
-    update_stats
+    $stats[:num_meetings] = $meetings.length
+    $stats[:num_users] = $meetings.inject(0) { |total, (k, v)| total + v[:users].length}
+    $stats[:num_bots] = $meetings.inject(0) { |total, (k, v)| total + v[:users].values.select { |u| u[:bot] }.length }
+    $stats[:num_voice_participants] = $meetings.inject(0) { |total, (k, v)| total + v[:users].values.select { |u| u[:voiceUser] }.length }
+    $stats[:num_voice_listeners] = $meetings.inject(0) { |total, (k, v)| total + v[:users].values.select { |u| u[:listenOnly] }.length }
+    $stats[:num_videos] = $meetings.inject(0) { |total, (k, v)| total + v[:users].inject(0) { |total, (k, v)| total + v[:videos].length}}
   end
 end
